9f57287f443d7562ef54052bb87f1de5e5957479
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-12 05:49:25 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  
+  board = [" O ", "X", " ", " ", "X", " ", "X", "O", " "]
+  display_board(board)
+
+end
