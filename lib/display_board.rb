b1701f92b23e6f395c5774c6edb1c0a20fbaf25b
Kevin Botero <kbotero201@gmail.com>
2020-06-28 00:30:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,22 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+#def display_board 
+ # puts "   |   |   "
+ # puts "-----------"
+ # puts "   |   |   "
+ # puts "-----------"
+ # puts "   |   |   "
+#end
+
+def display_board(person) 
+  puts " #{person[0]} | #{person[1]} | #{person[2]} "
+  puts "-----------"
+  puts " #{person[3]} | #{person[4]} | #{person[5]} "
+  puts "-----------"
+  puts " #{person[6]} | #{person[7]} | #{person[8]} "
+ 
+end
+
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+display_board(board)
